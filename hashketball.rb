af5e661c3d9022b95e46a8b9ed4dfc43a19dfb0c
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 22:17:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,7 +166,7 @@ def player_numbers(team)
   elsif game_hash[:away][:team_name] == team
     if game_hash[:home][:team_name] == team
       game_hash[:home][:players].each do |player_name, stat|
-        player_nums << player_name[:number]
+        player_nums << stat[:number]
       end
     end
   end
