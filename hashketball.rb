564b3a5d415685217265ed6e2843c48587a29cd3
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 20:58:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -116,18 +116,3 @@ def game_hash
 }
 }
 end
-
-def good_practices
-  game_hash.each do |location, team_data|
-      team_data.each do |attribute, data|
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-        data.each do |data_item|
-            binding.pry
-      end
-    end
-  end
-end
-
-good_practices
