3530ad01682d74de55010e43e71d409c01296e56
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 20:34:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,22 @@
 # Write your code here!
 def game_hash
+  hasketball = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => {
+        "name" => {
+          :number =>,
+          :shoe =>,
+          :points =>,
+          :rebounds =>,
+          :assists =>,
+          :steals =>,
+          :blocks =>,
+          :slam_dunks =>
+        }
+      }
+    }, 
+    :away = {}
+  }
 end
